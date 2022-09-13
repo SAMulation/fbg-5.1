2e786378397784a5c923d89fe88101b1e7f0f761
Sam Sarsten <sam.sarsten@gmail.com>
2022-09-13 06:10:42 -0700

    Player constructor finished (yet untested)

--- testing.js
+++ testing.js
@@ -1,10 +1,10 @@
 
 
 class Team {
-    constructor(name, city, abrv) {
+    constructor(name = 'Name', city = 'City', abrv = 'FBG') {
         this.name = name;
         this.city = city;
-        this.abrv = abrv;
+        this.abrv = abrv;    
     }
 
     get(attr) {
@@ -31,19 +31,77 @@ class Team {
 }
 
 class Player {
-    constructor(team, score=0, time=3, plays = null, mults = null, yards = null, stats = null) {
-        this.name = name;
-        this.city = city;
-        this.abrv = abrv;
+    // qtr gets passed at construction call
+    // minimum req: Player(team, qtr)
+    constructor(team, qtr, init = true, score = 0, time = 3, plays = null, mults = null, yards = null, stats = null) {
+        this.team = new Team(team.get('name'), team.get('city'), team.get('abrv'));
+        this.score = score;
+        this.timeouts = time;
+        this.plays = plays;
+        this.mults = mults;
+        this.yards = yards;
+        this.stats = stats;
+        this.currentPlay = '';
+
+        if (init) {
+            this.score = 0;
+            if (qtr < 5) {
+                this.timeouts = 3;
+            } else {
+                this.timeouts = 2;
+            }
+        }
+
+        if (!plays) {
+            this.fillPlays('a', qtr);
+        }
+
+        if (!mults) {
+            this.fillMults();
+        }
+
+        if (!plays) {
+            this.fillYards();
+        }
+
+        // LATER: Come up with Stats class
+        // if (!stats) {
+        //     this.stats = new Stats();
+        // }
     }
 
-    getTeam() {
-        return {
-            'name': this['name'],
-            'city': this['city'],
-            'abrv': this['abrv']
+    fillPlays(option, qtr = 4) {
+        // 3 hail marys by default per half
+        let hm = 3;
+        // If OT, only 2 hail marys
+        if (qtr > 4) {
+            hm = 2;
+        }
+
+        if (option === 'a') {
+            this.plays = [3, 3, 3, 3, 1, hm];
+        } else if (option === 'h') {
+            this.plays[5] = hm;  // Resetting hm only
         }
     }
+
+        // NEXT get, set, dec
+
+    fillMults() {
+        this.mults = [4, 4, 4, 3];
+    }
+
+    // NEXT get, set, dec
+
+    fillYards() {
+        this.yards = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    }
+
+        // NEXT get, set, dec
+
+
+
+
 
     get(attr) {
         return this[attr];
@@ -52,6 +110,22 @@ class Player {
     set(attr, value) {
         this[attr] = value;
     }
+
+    // getTeam() {
+    //     return {
+    //         'name': this['name'],
+    //         'city': this['city'],
+    //         'abrv': this['abrv']
+    //     }
+    // }
+
+    // get(attr) {
+    //     return this[attr];
+    // }
+
+    // set(attr, value) {
+    //     this[attr] = value;
+    // }
 }
 
 // class Player(object):
@@ -174,4 +248,7 @@ class Player {
 //         return self.__getattribute__("team").get(key)
 
 let testTeam = new Team('Rams', 'Los Angeles', 'LAR');
-console.log(testTeam.getTeam())
+console.log(testTeam.getTeam());
+
+let testPlayer = new Player(testTeam);
+console.log(testPlayer.get('team').getTeam());
