873211a2c3a0735a559bfa0aa0dab947addc28ca
Sam Sarsten <sam.sarsten@gmail.com>
2022-10-19 16:13:34 -0700

    Fixed js

--- js/refactor.js
+++ js/refactor.js
@@ -2,10 +2,10 @@ const hideField = () => {
   document.querySelector('.clock').addEventListener('click', event => {
     if (document.querySelector('.field-container').classList.contains('slide-away')) {
       document.querySelector('.field-container').classList.remove('slide-away')
-      document.querySelector('.field-container').style.display = 'block'
+    //   document.querySelector('.field-container').style.display = 'block'
     } else {
       document.querySelector('.field-container').classList.add('slide-away')
-      document.querySelector('.field-container').style.display = 'none'
+    //   document.querySelector('.field-container').style.display = 'none'
     }
   })
 }
